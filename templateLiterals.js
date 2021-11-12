420df418036a0f26b3687798adfacf3dcc010e80
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-12 09:42:30 -0600

    template literals

---
+++ templateLiterals.js
@@ -0,0 +1,24 @@
+let word1 = 'Dylan';
+ let word2 = 'Israel';
+
+// const fullName = word1 + ' ' + word2;
+//const fullName = `${word1} ${word2}`;
+
+ //console.log(fullName);
+
+let num1 = 2;
+let num2 = 3;
+
+////const fullName = `${num1 + num2} ${word2}`;
+
+// console.log(fullName);
+
+// let example = 'Hello \n' + 'world';
+
+let example = `${word1} 
+${word2}`;  // two lines
+
+let example = `${word1} ${word2}`; // one line
+
+console.log(example)
+document.getElementById('example').innerText = example;
