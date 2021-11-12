f2a08e1ce69cd0e1343106d359b115c26c6fe537
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-12 10:37:52 -0600

    destructuring array

---
+++ destructuringArrays.js
@@ -0,0 +1,5 @@
+let [firstName, middleName, lastName] = ['Dylan', 'Coding God', 'Israel'];
+
+lastName = 'Clements'; 
+console.log(lastName);
+console.log(firstName); // it will show as Clements as have destructured Array
